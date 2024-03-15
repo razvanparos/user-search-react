af938c3ba04199a02019d05f92b1fbc91562ee72
Razvan Paros <razvanparos21@gmail.com>
2024-03-15 17:11:25 +0200

    added theme change

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
     const [userData, setUserData] = useState();
     const [userEndpoint, setUserEndpoint] = useState('octocat');
     const [userSearch, setUserSearch] = useState('');
+    const [theme, setTheme] = useState(false);
     const [userNotFound, setUserNotFound] = useState(false);
     useEffect(()=>{
         axios.get(`https://api.github.com/users/${userEndpoint}`)
@@ -21,6 +22,13 @@ function Home() {
           
         
     },[userEndpoint])
+    useEffect(()=>{
+      if(theme===true){
+        document.body.style.backgroundColor = "white";
+      }else document.body.style.backgroundColor = "#141d2f";
+      
+        
+    },[theme])
 
     const handleSearchChange = (e) =>{
         setUserSearch(e.target.value)
@@ -34,18 +42,24 @@ function Home() {
 
     
   return (
-    <div className="home-div">
+    <div className={`home-div ${theme?'':'dark-theme'}`}>
       <div className='header'>
         <h1 className='logo'>devfinder</h1>
-        <button className='theme-switch-btn'>DARK <svg className="header-theme-icon" width="20" height="20" xmlns="http://www.w3.org/2000/svg" id="moon">
-            <path d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z" fill="#697C9A" fillRule="nonzero"></path>
-          </svg></button>
+        <button onClick={()=>{setTheme(!theme)}} className='theme-switch-btn'>{`${theme?'DARK':'LIGHT'}`} <svg className="header-theme-icon" width="20" height="20" xmlns="http://www.w3.org/2000/svg" id="moon">
+            <path className={`${theme?'':'hidden'}`} d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z" fill="#697C9A" fillRule="nonzero"></path>
+            <svg  className={`${theme?'hidden':''}`} width="20" height="20" xmlns="http://www.w3.org/2000/svg" id="sun">
+            <g fill="#FFF" filRule="nonzero">
+              <path d="M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z"></path>
+            </g>
+          </svg>
+          </svg>
+          </button>
       </div>
 
       <div className='search-bar-div'>
         <div className='search-logo-input'>
           <img src={searchIcon} alt="" />
-          <input type="text" value={userSearch} className='search-input' placeholder='Search GitHub username...' onChange={handleSearchChange}/>
+          <input type="text" value={userSearch} className={`search-input ${theme?'':'dark-theme'}`} placeholder='Search GitHub username...' onChange={handleSearchChange}/>
         </div>
         
         <button className='search-btn' onClick={handleSearch} >Search</button>
@@ -55,7 +69,7 @@ function Home() {
         <div className='main-div'> 
             <img src={userData.avatar_url} alt="" className='avatar'/>
             <div className='user-details-top'>
-                <p className='user-name'>{userData.name}</p>
+                <p className={`user-name ${theme?'':'dark-theme'}`}>{userData.name}</p>
                 <a className='user-login' href={`https://github.com/${userData.login}`} target='blank'>@{userData.login}</a>
                 <p className='joined'>Joined {(() => {
                   const date = new Date(userData.created_at);
@@ -73,15 +87,15 @@ function Home() {
             <div className='user-stats-div'>
                 <div className='stat'>
                     <p>Repos</p>
-                    <span>{userData.public_repos}</span>
+                    <span className={`${theme?'':'dark-theme'}`}>{userData.public_repos}</span>
                 </div>
                 <div className='stat'>
                     <p>Followers</p>
-                    <span>{userData.followers}</span>
+                    <span className={`${theme?'':'dark-theme'}`}>{userData.followers}</span>
                 </div>
                 <div className='stat'>
                     <p>Following</p>
-                    <span>{userData.following}</span>
+                    <span className={`${theme?'':'dark-theme'}`}>{userData.following}</span>
                 </div>
             </div>
             <div className='user-details-bottom'>
